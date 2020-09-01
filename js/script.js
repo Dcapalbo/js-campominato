ea42d474c7287a7177f1d23dd060ab2e96296e0e
Domenico Capalbo <capalbodomenico@gmail.com>
2020-09-01 22:22:23 +0200

    function-inserted
    js-campominato

--- js/script.js
+++ js/script.js
@@ -1,40 +1,69 @@
-// Il computer deve generare 16 numeri casuali tra 1 e 100.
-// I numeri non possono essere duplicati
-// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
-// L’utente non può inserire più volte lo stesso numero.
-// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
-// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
-// BONUS: (da fare solo se funziona tutto il resto)
-// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
 // con difficoltà 0 => tra 1 e 100
 // con difficoltà 1 =>  tra 1 e 80
 // con difficoltà 2 => tra 1 e 50
 
-// functions
 
-//1. Insert the random computer numbers and make a variable of it
-function randomNumber(min, max) {
-  return Math.floor(Math.random()* (max - min + 1) + min)
+//funzione che genera un numero casuale tra 1 e 100
+//funzione che dato un array e un numero mi controlla che il numero non sia già nell'array
+//creo variabile array che contiene i numeri bomba
+//ciclo che chiama la mia funzione di generazione dei numeri 16 volte  while(i<bombe)
+    //check che il numero non sia duplicato
+    //se non è duplicato, lo salvo nell'array bombe
+//creo variabile array che contiene i numeri utente
+//variabili booleana haPerso = false
+//ciclo di 100-16 volte  //for(i=0; i<tentativi-bombe; i++)
+  //prompt in cui chiedo all'utente un numero
+  //check che il numero sia compreso tra 1 e 100
+  //check che il numero non sia duplicato nell'array utente
+  //se non è duplicato, salvo il numero nell'array utente
+  //check che il numero non sia nell'array bomba
+  //se il numero è nell'array bomba -> alert hai perso
+      //alert con array utente .length -> numero di volte che l'utente ha inserito un numero corretto
+      //haPerso = true;
+      //esco dal ciclo
+//controllo se haPerso è false
+  //alert hai vinto
+
+// 1. make a function which generate a number between 1 and 100
+function generateRandomNumber(min, max) {
+  var randomNumber = Math.floor(Math.random()* (max - min +1)) + min;
+  return randomNumber;
 }
+//2. make a variable to insert the function inside of it
+var randomNumber = generateRandomNumber(1, 100);
 
-//2. make numbers variables
-
-var computerNumber= randomNumber(1,100);
-var maxNumber = 100;
-var midNumber = 80
-var minNumber = 50
-
-//3. ask the utent number with a prompt
-
-var utentNumber = parseInt(prompt("Inserire un numero"));
-
-//4. make a var cycle to find
-
-for (var i = 0; i <= 84; i++) {
-  if (utentNumber <= 84) {
+//3. make a function to prevent choosing the same number more than once
+function insideArray(array, number) {
+  if (array.indexOf(number)) {
+    return true;
+  } else {
+    return false;
   }
-  console.log(utentNumber);
 }
 
-//.2
+function arrayRandomNumbers(quantityElementGen, minNumGen, maxNumGen) {
+  var numbersArray = [];
+  for (var i = 0; i < quantityElementGen; i++) {
+    var  = randomNumber(minNumber, maxNumber);
+    while (insideArray(numbersArray, tempRandomNumber)) {
+      totalRandomNumber = generateRandomNumber(minNumber, maxNumber);
+    }
+    numbersArray.push(totalRandomNumber);
+  }
+  return numbersArray;
+}
+
+alert("Welcome in the mainfield, the game will start if you click on this message")
+var difficulty = prompt("Choose a difficulty between: \"easy\", \"medium\" or \"hard\"");
+
+while ((difficulty != "easy") && (difficulty != "medium") && (difficulty != "hard")) {
+  difficulty = prompt("scrivi \"facile\", \"media\" o \"difficile\" per selezionare la difficoltà!!!");
+}
+
+if (difficulty == "easy") {
+  var maxNumber = 100;
+} else if (difficulty == "medium") {
+  var maxNumber = 80;
+} else if (difficulty == "hard") {
+  var maxNumber = 50;
+}
