af32f8f48b12e3ec376ad6ea3fb166b76c9907cd
Domenico Capalbo <capalbodomenico@gmail.com>
2020-09-07 15:44:09 +0200

    exercise-complete-with-bonus
    js-campominato

--- js/script.js
+++ js/script.js
@@ -1,69 +1,89 @@
-// con difficoltà 0 => tra 1 e 100
-// con difficoltà 1 =>  tra 1 e 80
-// con difficoltà 2 => tra 1 e 50
 
+//1. Make an alert as an intro to the game
 
-//funzione che genera un numero casuale tra 1 e 100
-//funzione che dato un array e un numero mi controlla che il numero non sia già nell'array
-//creo variabile array che contiene i numeri bomba
-//ciclo che chiama la mia funzione di generazione dei numeri 16 volte  while(i<bombe)
-    //check che il numero non sia duplicato
-    //se non è duplicato, lo salvo nell'array bombe
-//creo variabile array che contiene i numeri utente
-//variabili booleana haPerso = false
-//ciclo di 100-16 volte  //for(i=0; i<tentativi-bombe; i++)
-  //prompt in cui chiedo all'utente un numero
-  //check che il numero sia compreso tra 1 e 100
-  //check che il numero non sia duplicato nell'array utente
-  //se non è duplicato, salvo il numero nell'array utente
-  //check che il numero non sia nell'array bomba
-  //se il numero è nell'array bomba -> alert hai perso
-      //alert con array utente .length -> numero di volte che l'utente ha inserito un numero corretto
-      //haPerso = true;
-      //esco dal ciclo
-//controllo se haPerso è false
-  //alert hai vinto
+  alert("Welcome into the game, click to continue");
 
-// 1. make a function which generate a number between 1 and 100
+//2. make a prompt to select the difficulty
+
+  let difficulty = prompt("Select the difficulty \"easy\", \"medium\" or \"hard\"").toLowerCase();
+
+//3. make some condiction with a ciclw while, to select the difficulty
+
+  while ((difficulty != "easy") && (difficulty != "medium") && (difficulty != "hard")) {
+    difficulty = prompt("please write \"easy\", \"medium\" or \"hard\" to select the difficulty!").toLowerCase();
+  }
+
+//4. select the difficulty
+
+switch (difficulty) {
+  case "easy":
+   maxNumber = 100
+   break;
+  case "medium":
+   maxNumber = 80
+   break;
+   default:"hard"
+   maxNumber = 50
+   break;
+}
+
+//5. make some variables usefull for later.
+
+var insideNumbers = [];
+var isBomb = false;
+var bomb = 16;
+var range = maxNumber - bomb;
+
+//6. make a function to check inside of the array if there is an element or not
+
+function inArray(array, element) {
+  var i = 0;
+  while (i < array.length) {
+    if (array[i] == element) {
+      return true;
+    }
+    i++;
+  } return false;
+}
+
+//7. make a function to generate a random number
+
 function generateRandomNumber(min, max) {
-  var randomNumber = Math.floor(Math.random()* (max - min +1)) + min;
+  var randomNumber = Math.floor(Math.random() * (max - min) ) + 1;
   return randomNumber;
 }
-//2. make a variable to insert the function inside of it
-var randomNumber = generateRandomNumber(1, 100);
 
-//3. make a function to prevent choosing the same number more than once
-function insideArray(array, number) {
-  if (array.indexOf(number)) {
-    return true;
+//8. make  a while cicle to look inside of the bomblist array, if the number is not already inside the array, push it in.
+
+var bombList = [];
+while (bombList.length < bomb) {
+  var bombNumb = generateRandomNumber(1, maxNumber);
+  if (inArray(bombList, bombNumb) == false) {
+  bombList.push(bombNumb)
+  }
+} console.log(bombList);
+
+//9. make a whilce cicle to let the user insert the numbers with some conditions, if the utent number is the same of the bomb, make an alert, if the utent number is not inside of the array, push the utent number into it.
+
+while (insideNumbers.length < range && isBomb == false) {
+
+  var userNumber = parseInt(prompt("insert a number from 1 to " + maxNumber));
+  if (isNaN(userNumber) || userNumber > maxNumber || userNumber < 1) {
+    alert("You must insert a number from 1 to + " + maxNumber)
+  } else if (inArray(insideNumbers, userNumber) == true) {
+    alert("this number is already inserted");
   } else {
-    return false;
-  }
-}
-
-function arrayRandomNumbers(quantityElementGen, minNumGen, maxNumGen) {
-  var numbersArray = [];
-  for (var i = 0; i < quantityElementGen; i++) {
-    var  = randomNumber(minNumber, maxNumber);
-    while (insideArray(numbersArray, tempRandomNumber)) {
-      totalRandomNumber = generateRandomNumber(minNumber, maxNumber);
+    if (inArray(bombList, userNumber) == true) {
+      isBomb = true
+    } else {
+      insideNumbers.push(userNumber);
     }
-    numbersArray.push(totalRandomNumber);
   }
-  return numbersArray;
 }
+//10. Make a condition, if the bomb has found, send an alert, if the utent win the game, send an other one.
 
-alert("Welcome in the mainfield, the game will start if you click on this message")
-var difficulty = prompt("Choose a difficulty between: \"easy\", \"medium\" or \"hard\"");
-
-while ((difficulty != "easy") && (difficulty != "medium") && (difficulty != "hard")) {
-  difficulty = prompt("scrivi \"facile\", \"media\" o \"difficile\" per selezionare la difficoltà!!!");
-}
-
-if (difficulty == "easy") {
-  var maxNumber = 100;
-} else if (difficulty == "medium") {
-  var maxNumber = 80;
-} else if (difficulty == "hard") {
-  var maxNumber = 50;
+if (isBomb == true) {
+  alert("You lost, here your score level " + insideNumbers.length);
+} else {
+  alert("You win, well done!")
 }
